04d1ff1ae2f7bece1f088f6edeb8c9ba0a4aba3b
protivotankovaiaystanovka <navamelim.gmail.com>
2024-10-18 13:38:47 +0900

    add burger

---
+++ styles/main.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const close = document.querySelector('.close')
+
+burger.addEventListener('click', ()=>{
+    menu.classList.add('menu_open')
+
+    close.addEventListener('click', ()=>{
+        menu.classList.remove('menu_open')
+})
+
+
+}
+    
+)
