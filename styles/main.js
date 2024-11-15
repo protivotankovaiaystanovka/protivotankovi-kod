97eb0b44404bae7269a800970565248c7f7a37ce
protivotankovaiaystanovka <navamelim.gmail.com>
2024-11-15 13:24:29 +0900

    swiper

--- styles/main.js
+++ styles/main.js
@@ -1,3 +1,9 @@
+import Swiper from "swiper";
+import "swiper/css";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
 const close = document.querySelector('.close')
@@ -8,8 +14,22 @@ burger.addEventListener('click', ()=>{
     close.addEventListener('click', ()=>{
         menu.classList.remove('menu_open')
 })
-
-
 }
-    
 )
+
+Swiper.use([Pagination]);
+const swiper = new Swiper(".swiper", {
+  slidesPerView: "auto", // сколько слайдов показывать
+  spaceBetween: 16,
+  observer: true, // проверка находится ли элемент в области видимости
+  breakpoints: {
+    1119: {
+      enabled: false,
+    }, // значение когда нужно сломать swiper
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "bullets",
+  },
+});
